719bf0b6e1ad27c482fc954ab86e74e46692c6bf
Pierce Conwi <pierceconwi@gmail.com>
2023-05-07 14:40:31 -0700

    Troubleshooting apostrophes

--- pages/components/SplashBox.jsx
+++ pages/components/SplashBox.jsx
@@ -14,7 +14,7 @@ export default function SplashBox() {
                 <Col sm="12" md="8">
                     <h1>Pierce Conwi</h1>
                     <h2>Web Developer</h2>
-                    <p>Hi, I’m Pierce, a web developer located in the San Francisco Bay Area. I am a resilient team player with a strong analytical mind who welcomes every new challenge with enthusiasm.</p>
+                    <p>Hi, I&apos;m Pierce, a web developer located in the San Francisco Bay Area. I am a resilient team player with a strong analytical mind who welcomes every new challenge with enthusiasm.</p>
                     <p>Let my web expertise go to work for you!</p>
                 </Col>
                 <Col sm="12" md="4">
