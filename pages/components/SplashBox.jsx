fa8ec36a0a86182df2f47e9b53e471cd8ed1ada0
Pierce Conwi <pierceconwi@gmail.com>
2023-05-08 09:54:01 -0700

    Continued testing

--- pages/components/SplashBox.jsx
+++ pages/components/SplashBox.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import selfie from '../public/hs_full.jpg';
+import selfie from 'public/hs_full.jpg';
 
 export default function SplashBox() {
     return (
